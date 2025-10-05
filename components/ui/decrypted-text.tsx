31d761e3e9f57e7cb28fd780f2af5cee6a8aa10f
Saurabh <Saurabhkarma2004@gmail.com>
2025-10-05 18:30:07 +0530

    Update decrypted-text.tsx

--- components/ui/decrypted-text.tsx
+++ components/ui/decrypted-text.tsx
@@ -42,7 +42,7 @@ export default function DecryptedText({
   sequential = false,
   revealDirection = "start",
   useOriginalCharsOnly = false,
-  characters = "!@#$%^&*()_+",
+  characters = "Meenaskhi",
   className = "",
   parentClassName = "",
   encryptedClassName = "",
